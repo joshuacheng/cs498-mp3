34bdcdfd1970c85aaa3ecd1797fea5a003d03a4c
Joshua Cheng <joshua.bj.cheng@gmail.com>
2019-03-20 14:03:44 -0700

    Add basic post for /tasks

--- models/task.js
+++ models/task.js
@@ -2,12 +2,12 @@ var mongoose = require('mongoose');
 
 var TaskSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    description: String,
+    description: { type: String, default: ""},
     deadline: { type: Date, required: true },
-    completed: Boolean,
+    completed: { type: Boolean, default: false },
     assignedUser: { type: String, default: "" },
     assignedUserName: { type: String, default: "unassigned"},
     dateCreated: { type: Date, default: Date.now } 
-}, {versionKey: false}) // not recommended
+}, {versionKey: false}) // not recommended but gets rid of __v
 
 module.exports = mongoose.model('Task', TaskSchema);
--- routes/tasks.js
+++ routes/tasks.js
@@ -18,5 +18,26 @@ router.get('/', function (req, res) {
     })
 })
 
+router.post('/', function (req, res) {
+    if (req.body.name && req.body.deadline) {
+        console.log('success');
+        tasks.create({
+            name: req.body.name,
+            deadline: req.body.deadline
+        }, (err, createdTask) => {
+            res.status(200).send({
+                message: 'OK',
+                data: createdTask
+            })
+        })
+        
+    } else {
+        res.status(404).send({
+            message: 'Task requires name and deadline',
+            data: {}
+        })
+    }
+})
+
 
 module.exports = router;
